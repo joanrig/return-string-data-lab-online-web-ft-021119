d27007d028b637cbfd22da2eda18e1be72116fce
github username <github email address>
2019-06-06 22:37:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'home', to: 'static#home'
+  get 'home', to: 'products#index'
   resources :orders
   resources :invoices
+  resources :products
+  get '/products/:id/body', to: 'products#body'
+  
+  get 'products/:id/inventory', to: 'products#inventory'
+  get 'products/:id/description', to: 'products#description'
 end
